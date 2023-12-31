cd7e1d01707e8d1a1605b8ed38bcc9e25505baee
kuma <kumaleta2021@gmail.com>
2023-12-31 15:58:12 +0300

    another commit

--- back-end/index.js
+++ back-end/index.js
@@ -1,63 +1,8 @@
 const express=require('express')
-const app=express()
 const mongoose=require('mongoose')
-mongoose.connect(process.env.CONNECTION_STRING.replace('<password>',process.env.PASSWORD))
-const port=8000
-// const jwt=require('jsonwebtoken')
-// const secretKey='secret'
-// const payload={
-//     username:"kleta",
-//     password:123
-// }
-// const token=jwt.sign(payload,secretKey,{expiresIn:'1h'})
-// console.log('generated jwt :',token)
-const userAuthenticationData = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-const userData=mongoose.model('userData',)
-const adminSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
-
-const employeeSchema = mongoose.Schema({
-  date: {
-    type: String,
-    required: true
-  },
-  firstName: {
-    type: String,
-    required: true
-  },
-  middleName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  socialSecurityNumber:{
-    type:String,
-    required:true
-  }
-  
-});
-const model=mongoose.model('employeeData',DbSchema)
-// app.post('/')
-app.listen(port,()=>{
-    console.log(`the server is running on port ${port}`)
+require('dotenv').config();
+const app=express();
+console.log(process.env)
+app.listen(process.env.PORT,()=>{
+    console.log(`the server is running on port ${process.env.PORT}`);
 })
